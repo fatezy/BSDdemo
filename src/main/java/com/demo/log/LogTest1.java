ea1140a32de8decf799b706e410e2e1ea1909cf2
fatezy <1104199203@qq.com>
2019-01-17 23:26:59 +0800

    修改log配置

--- src/main/java/com/demo/log/LogTest1.java
+++ src/main/java/com/demo/log/LogTest1.java
@@ -1,7 +1,7 @@
 package com.demo.log;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Hello world!
@@ -9,15 +9,23 @@ import org.apache.logging.log4j.Logger;
  */
 public class LogTest1
 {
+    private static Logger logger = LoggerFactory.getLogger(LogTest1.class);
+
     public static void main(String[] args) {
 //        Logger logger = LogManager.getLogger("Root");
-        Logger logger = LogManager.getLogger("mylog");
+//        Logger logger = LogManager.getLogger("mylog");
+
         logger.trace("trace level");
         logger.debug("debug level");
         logger.info("info level");
         logger.warn("warn level");
         logger.error("error level");
-        logger.fatal("fatal level");
+        test();
+    }
+
+
+    public static void test(){
+        logger.info("test");
     }
 
 }
